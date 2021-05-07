97350f05aef02f4e3beb5f3593b64ae19de9b9e9
nlao1 <nlao@seas.upenn.edu>
2021-05-06 17:59:58 -0700

    added starter code for main.java

--- spotify-recommender/src/Main.java
+++ spotify-recommender/src/Main.java
@@ -1,6 +1,14 @@
+import com.wrapper.spotify.SpotifyApi;
+
+import auth.Authorization;
 
 public class Main {
     public static void main(String[] args) {
+        Authorization authorizer = new Authorization();
+        System.out.println(authorizer.getAuthorizationURI());
+        String code = authorizer.handleRedirectUri();
+        SpotifyApi spotifyApi = authorizer.apiFromCode(code);
         
+        //add user functionality here
     }
 }
