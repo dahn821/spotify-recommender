580f4388f654748f059360971514312c513d02d0
nlao1 <nlao@seas.upenn.edu>
2021-05-06 17:17:07 -0700

    added getUserPlaylists method (probably useless)

--- spotify-recommender/src/Recommender.java
+++ spotify-recommender/src/Recommender.java
@@ -1,16 +1,42 @@
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.hc.core5.http.ParseException;
+
 import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.exceptions.detailed.ForbiddenException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.PlaylistSimplified;
 import com.wrapper.spotify.model_objects.specification.Track;
-import com.wrapper.spotify.model_objects.specification.User;
 
 public class Recommender {
-    SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setAccessToken("taHZ2SdB-bPA3FsK3D7ZN5npZS47cMy-IEySVEGttOhXmqaVAIo0ESvTCLjLBifhHOHOIuhFUKPW1WMDP7w6dj3MAZdWT8CLI2MkZaXbYLTeoDvXesf2eeiLYPBGdx8tIwQJKgV8XdnzH_DONk")
-            .build();
     
-    public static List<Track> getRecommendedTracks(User user) {
-        
+    SpotifyApi spotifyApi;
+    
+    public Recommender(SpotifyApi spotifyApi) {
+        this.spotifyApi = spotifyApi;
     }
+    
+    /**
+     * Does not need any parameters because the api object itself contains the access code
+     * @return
+     */
+    public List<Track> recommendedTracks() {
+        //consider favorite artists
+    }
+    
+    public Paging<PlaylistSimplified> getUserPlaylists() {
+        try {
+            return spotifyApi.getListOfCurrentUsersPlaylists().build().execute();
+        } catch (ParseException | SpotifyWebApiException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    
     
 }
