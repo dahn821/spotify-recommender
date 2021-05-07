7d7db0c41cc524e4e8c5bc1dd70c4c0cefa7cf10
Tony An <40644135+tonyjan02@users.noreply.github.com>
2021-05-06 23:24:39 -0400

    Update Main.java

--- spotify-recommender/src/Main.java
+++ spotify-recommender/src/Main.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 import auth.Authorization;
 
 public class Main {
+    
+    public void ask() {
+        System.out.println("What recommendation would you like?");
+        System.out.println("Enter 1 for Songs, 2 for Artists, 3 for Playlists);
+        Scanner in = new Scanner(System.in);
+        int desiredRec = Integer.parseInt(in.next());
+                           
+        if (desiredRec == 1) {
+            
+        } else if (desiredRec == 2) {
+            
+        } else if (desiredRec == 3) {
+            getPlaylist(userId);                   
+        }
+        
+                           
+    }
     public static void main(String[] args) {
         // links user's account
         Authorization authorizer = new Authorization();
@@ -11,17 +28,6 @@ public class Main {
         SpotifyApi spotifyApi = authorizer.apiFromCode(code);
         
         // recommendation process
-        System.out.println("What recommendation would you like?");
-        System.out.println("Enter 1 for Songs, 2 for Artists, 3 for Playlists);
-        Scanner in = new Scanner(System.in);
-        int desiredRec = Integer.parseInt(in.next());               
-        
-        if (desiredRec == 1) {
-            
-        } else if (desiredRec == 2) {
-            
-        } else if (desiredRec == 3) {
-            
-        }
+        ask();
     }
 }
