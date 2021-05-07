1d083ee173baaca6a96ab26b079d4aadb9570f30
Tony An <40644135+tonyjan02@users.noreply.github.com>
2021-05-06 23:36:55 -0400

    included playlist recommendation ui

--- spotify-recommender/src/Main.java
+++ spotify-recommender/src/Main.java
@@ -5,17 +5,24 @@ import auth.Authorization;
 public class Main {
     
     public void ask() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("*All inputs are case sensitive*");
+        System.out.println("What is your Spotify username/ID");
+        String username = in.nextLine();
+        
         System.out.println("What recommendation would you like?");
         System.out.println("Enter 1 for Songs, 2 for Artists, 3 for Playlists);
-        Scanner in = new Scanner(System.in);
-        int desiredRec = Integer.parseInt(in.next());
+        int desiredRec = Integer.parseInt(in.nextLine());
                            
         if (desiredRec == 1) {
             
         } else if (desiredRec == 2) {
             
         } else if (desiredRec == 3) {
-            getPlaylist(userId);                   
+            System.out.println("Do you want a recommendation from the current top feature playlists or would you like to choose a genre?");
+            System.out.println("(type g for genre or f for featured)");
+		    String type = in.nextLine(); 
+            PlaylistSimilarity.getPlaylist(spotifyApi, username, type);                   
         }
         
                            
